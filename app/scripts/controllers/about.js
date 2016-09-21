700e84571754e292e4fdd3297c9815ef9a97d653
Shane Starr <starr749@gmail.com>
2016-09-20 22:58:57 -0500

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name firestarterApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the firestarterApp
+ */
+angular.module('firestarterApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name firestarterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the firestarterApp
+ */
+angular.module('firestarterApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
