eb303e488ee2a82bc3acad5148acc261f63cc8c8
Shane Starr <shane.starr@gm.com>
2018-01-29 17:56:35 -0600

    adds stance to fight!

--- app/scripts/controllers/fight.js
+++ app/scripts/controllers/fight.js
@@ -12,13 +12,44 @@ angular.module('firestarterApp')
 
     var ctrl = this;
 
+    $scope.aggressiveStance = { "name": "aggressive", "text":  "Aggressive stance grants +2D to Strike and Great " +
+      "Strike. Block and Counterstrike suffer +2 Ob penalty. You may not " +
+      "Avoid. If you accidently script Avoid while in aggressive stance, " +
+      "you hesitate for all action."};
+    $scope.neutralStance = {"name": "neutral", "text": "Neutral stance is the default. You start a fight in neutral " +
+      "stance unless otherwise noted. It grants no advantage and suffers no " +
+      "disadvantages." };
+    $scope.defensiveStance = { "name": "defensive", "text": "Defensive stance grants +2D to Avoid, Block and " +
+      "Counterstrike. Strike and Great Strike suffer a +2 Ob penalty when " +
+      "performed from defensive stance."};
+
+
+
+    $scope.player1 = {};
+    $scope.player2 = {};
+
     ctrl.actions = ACTIONS.fight;
     ctrl.interactions = INTERACTIONS.fight;
 
     ctrl.player1int = "";
     ctrl.player2int = "";
 
+    $scope.player1.stance = $scope.neutralStance;
+    $scope.player2.stance = $scope.neutralStance;
+
+    $scope.changeStance = function(newStance) {
+      console.log(newStance);
+      $scope.player1.stance = newStance;
+    };
+
+    $scope.changeStanceP2 = function(newStance) {
+      $scope.player2.stance = newStance;
+    };
+
     $scope.update = function() {
+
+      console.log($scope.player2);
+
       if ((ctrl.p1Action === undefined) || (ctrl.p2Action === undefined)) {
         return;
       }
